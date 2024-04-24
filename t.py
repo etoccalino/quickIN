a744f2c06630df2bbcdc6ac810c4ec126573434e
Elvio Toccalino <etoccalino@covariant.ai>
2024-04-24 15:01:04 -0700

    GUI prototypes and Trello API docs

---
+++ t.py
@@ -0,0 +1,11 @@
+import FreeSimpleGUI as sg
+
+
+layout = [ [sg.Text('Hello, world!')] ]
+window = sg.Window('Hello Example', layout)
+while True:
+    event, values = window.read()
+    if event == sg.WIN_CLOSED:
+        break
+
+window.close()
